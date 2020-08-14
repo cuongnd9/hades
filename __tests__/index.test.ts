e8319f418dbf41e6ea05dd354ce2008af3276769
Cuong Duy Nguyen <103cuong@gmail.com>
2020-08-14 21:41:41 +0700

    feat: v0.1.0

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,3 +1,26 @@
-it('test', async() => {
+import {hades} from '../src';
+
+it('download image', async() => {
+  await hades({
+    url: 'https://kenh14cdn.com/thumb_w/660/2020/3/12/hd1-1583989092728481468447.jpg',
+    path: `${__dirname}/images/image.jpg`,
+  });
+  expect(true).toEqual(true);
+});
+
+it('download gif image', async() => {
+  await hades({
+    url: 'https://raw.githubusercontent.com/103cuong/103cuong/master/banner.gif',
+    path: `${__dirname}/images/banner.gif`,
+    timeout: 7000,
+  });
+  expect(true).toEqual(true);
+});
+
+it('download go file', async() => {
+  await hades({
+    url: 'https://raw.githubusercontent.com/103cuong/uid/master/uid.go',
+    path: `${__dirname}/images/uid.go`,
+  });
   expect(true).toEqual(true);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-import { logger } from 'juno-js';
+import axios from 'axios';
+import {createWriteStream} from 'fs';
 
-logger().info('Chao xìn 🇻🇳');
+type Options = {
+  url: string;
+  path: string;
+  timeout?: number;
+};
+async function hades(options: Options) {
+  const writer = createWriteStream(options.path);
+
+  const response = await axios({
+    url: options.url,
+    method: 'GET',
+    responseType: 'stream',
+    timeout: options.timeout,
+  });
+
+  response.data.pipe(writer);
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve);
+    writer.on('error', reject);
+  });
+}
+
+export {hades};
